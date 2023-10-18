57075642b3adf065f25f841f959941ea50556bb9
MiloszRolewski <milosz.rolewski01@gmail.com>
2023-10-18 10:40:54 +0200

    notebook field is working +/-, navigation buttons added

--- app/src/main/java/com/example/notebookapp/NotebookActivity.java
+++ app/src/main/java/com/example/notebookapp/NotebookActivity.java
@@ -2,19 +2,37 @@ package com.example.notebookapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class NotebookActivity extends AppCompatActivity {
-    Button add_button;
-    Button settings_button;
-    Button save_button;
+
+    Button b_change_password;
+    Button b_save_note;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_notebook);
 
+        b_change_password = (Button) findViewById(R.id.b_change_password);
+        b_save_note = (Button) findViewById(R.id.b_save_note);
+
+        b_change_password.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                System.out.println("b_change_password");
+            }
+        });
+
+        b_save_note.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                System.out.println("b_save_note");
+            }
+        });
 
 
     }
